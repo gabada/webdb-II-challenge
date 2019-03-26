e816e867de4202f200c5687c7e9e4f6ead8fc7a1
Gill Abada <gill@callifi.com>
2019-03-26 10:00:30 -0400

    GET by ID endpoint completed.

--- index.js
+++ index.js
@@ -26,6 +26,19 @@ server.get('/api/zoos', async (req, res) => {
   }
 });
 
+server.get('/api/zoos/:id', async (req, res) => {
+  try {
+    const zoo = await db('zoos').where({ id: req.params.id });
+    if (zoo.length < 1) {
+      res.status(404).json({ message: 'The selected Zoo does not exist' });
+    } else {
+      res.status(200).json(zoo);
+    }
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
 const port = 3300;
 server.listen(port, function() {
   console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
