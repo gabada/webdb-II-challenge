768bf93b0b7ab170f90b591c28be6bed77967adc
Gill Abada <gill@callifi.com>
2019-03-26 10:36:15 -0400

    complete PUT and DELETE endpoints. MVP complete.

--- index.js
+++ index.js
@@ -58,6 +58,39 @@ server.get('/api/zoos/:id', async (req, res) => {
   }
 });
 
+server.put('/api/zoos/:id', async (req, res) => {
+  try {
+    const count = await db('zoos')
+      .where({ id: req.params.id })
+      .update(req.body);
+    if (count > 0) {
+      const zoo = await db('zoos')
+        .where({ id: req.params.id })
+        .first();
+      res.status(200).json(zoo);
+    } else {
+      res.status(404).json({ message: 'Record not found' });
+    }
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+server.delete('/api/zoos/:id', async (req, res) => {
+  try {
+    const count = await db('zoos')
+      .where({ id: req.params.id })
+      .del();
+    if (count > 0) {
+      res.status(204).end();
+    } else {
+      res.status(404).json({ message: 'Record not found' });
+    }
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
 const port = 3300;
 server.listen(port, function() {
   console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
